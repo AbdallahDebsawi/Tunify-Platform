8a8076186daceac1bc9876ddda08c3b931701382
AbdallahDebsawi <96738825+AbdallahDebsawi@users.noreply.github.com>
2024-08-25 19:52:02 +0300

    edit routs

---
+++ Tunify-Platform/Repositories/Interfaces/IAccount.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Tunify_Platform.Models.DTO;
+
+namespace Tunify_Platform.Repositories.Interfaces
+{
+    public interface IAccount
+    {
+        // Add register
+        public Task<UserDto> Register(RegisterDto registerdUserDto, ModelStateDictionary modelState);
+
+        // Add login 
+        public Task<UserDto> Login(string username, string password);
+
+        // Add logout 
+        public Task Logout();
+    }
+}
---
+++ Tunify-Platform/Repositories/Services/IdentityAccountService.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Tunify_Platform.Models;
+using Tunify_Platform.Models.DTO;
+using Tunify_Platform.Repositories.Interfaces;
+
+namespace Tunify_Platform.Repositories.Services
+{
+    public class IdentityAccountService : IAccount
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+
+        public IdentityAccountService(UserManager<ApplicationUser> Manager)
+        {
+            _userManager = Manager;
+
+        }
+        public async Task<UserDto> Login(string username, string password)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+
+            bool passValidation = await _userManager.CheckPasswordAsync(user, password);
+
+            if (passValidation)
+            {
+                return new UserDto()
+                {
+                    Id = user.Id,
+                    Username = user.UserName
+                };
+            }
+
+            return null;
+        }
+
+        public async Task Logout()
+        {
+            try
+            {
+                await _signInManager.SignOutAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        public async Task<UserDto> Register(RegisterDto registerdUserDto, ModelStateDictionary modelState)
+        {
+            var user = new ApplicationUser()
+            {
+                UserName = registerdUserDto.UserName,
+                Email = registerdUserDto.Email,
+
+            };
+
+            var result = await _userManager.CreateAsync(user, registerdUserDto.Password);
+
+            if (result.Succeeded)
+            {
+                return new UserDto()
+                {
+                    Id = user.Id,
+                    Username = user.UserName
+                };
+            }
+
+            foreach (var error in result.Errors)
+            {
+                var errorCode = error.Code.Contains("Password") ? nameof(registerdUserDto) :
+                                error.Code.Contains("Email") ? nameof(registerdUserDto) :
+                                error.Code.Contains("Username") ? nameof(registerdUserDto) : "";
+
+                modelState.AddModelError(errorCode, error.Description);
+            }
+
+
+            return null;         
+        }
+    }
+}
